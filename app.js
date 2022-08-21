c16aebce8c4b4fa745157bd87888289737b052b8
Adeyomola <kayzeel@yahoo.com>
2022-08-21 19:37:12 +0100

    Added 3hourly temp readings, removed humidity and pressure readings

--- app.js
+++ app.js
@@ -11,44 +11,59 @@ const wind = document.getElementById("wind");
 const visibility = document.getElementById("visibility");
 // nextFiveDays DOM
 const tomorrow = document.getElementById("tomorrow");
-const tomorrowAverage = document.getElementById("tomorrowAverage");
-const tomorrowHigh = document.getElementById("tomorrowHigh");
-const tomorrowLow = document.getElementById("tomorrowLow");
+const tomorrowTemp1 = document.getElementById("tomorrowTemp1");
+const tomorrowTemp2 = document.getElementById("tomorrowTemp2");
+const tomorrowTemp3 = document.getElementById("tomorrowTemp3");
+const tomorrowTemp4 = document.getElementById("tomorrowTemp4");
+const tomorrowTemp5 = document.getElementById("tomorrowTemp5");
+const tomorrowTemp6 = document.getElementById("tomorrowTemp6");
+const tomorrowTemp7 = document.getElementById("tomorrowTemp7");
+const tomorrowTemp8 = document.getElementById("tomorrowTemp8");
 const tomorrowDescription = document.getElementById("tomorrowDescription");
-const tomorrowHumidity = document.getElementById("tomorrowHumidity");
-const tomorrowPressure = document.getElementById("tomorrowPressure");
 
 const day2 = document.getElementById("day2");
-const day2Average = document.getElementById("day2Average");
-const day2High = document.getElementById("day2High");
-const day2Low = document.getElementById("day2Low");
+const day2Temp1 = document.getElementById("day2Temp1");
+const day2Temp2 = document.getElementById("day2Temp2");
+const day2Temp3 = document.getElementById("day2Temp3");
+const day2Temp4 = document.getElementById("day2Temp4");
+const day2Temp5 = document.getElementById("day2Temp5");
+const day2Temp6 = document.getElementById("day2Temp6");
+const day2Temp7 = document.getElementById("day2Temp7");
+const day2Temp8 = document.getElementById("day2Temp8");
 const day2Description = document.getElementById("day2Description");
-const day2Humidity = document.getElementById("day2Humidity");
-const day2Pressure = document.getElementById("day2Pressure");
 
 const day3 = document.getElementById("day3");
-const day3Average = document.getElementById("day3Average");
-const day3High = document.getElementById("day3High");
-const day3Low = document.getElementById("day3Low");
+const day3Temp1 = document.getElementById("day3Temp1");
+const day3Temp2 = document.getElementById("day3Temp2");
+const day3Temp3 = document.getElementById("day3Temp3");
+const day3Temp4 = document.getElementById("day3Temp4");
+const day3Temp5 = document.getElementById("day3Temp5");
+const day3Temp6 = document.getElementById("day3Temp6");
+const day3Temp7 = document.getElementById("day3Temp7");
+const day3Temp8 = document.getElementById("day3Temp8");
 const day3Description = document.getElementById("day3Description");
-const day3Humidity = document.getElementById("day3Humidity");
-const day3Pressure = document.getElementById("day3Pressure");
 
 const day4 = document.getElementById("day4");
-const day4Average = document.getElementById("day4Average");
-const day4High = document.getElementById("day4High");
-const day4Low = document.getElementById("day4Low");
+const day4Temp1 = document.getElementById("day4Temp1");
+const day4Temp2 = document.getElementById("day4Temp2");
+const day4Temp3 = document.getElementById("day4Temp3");
+const day4Temp4 = document.getElementById("day4Temp4");
+const day4Temp5 = document.getElementById("day4Temp5");
+const day4Temp6 = document.getElementById("day4Temp6");
+const day4Temp7 = document.getElementById("day4Temp7");
+const day4Temp8 = document.getElementById("day4Temp8");
 const day4Description = document.getElementById("day4Description");
-const day4Humidity = document.getElementById("day4Humidity");
-const day4Pressure = document.getElementById("day4Pressure");
 
 const day5 = document.getElementById("day5");
-const day5Average = document.getElementById("day5Average");
-const day5High = document.getElementById("day5High");
-const day5Low = document.getElementById("day5Low");
+const day5Temp1 = document.getElementById("day5Temp1");
+const day5Temp2 = document.getElementById("day5Temp2");
+const day5Temp3 = document.getElementById("day5Temp3");
+const day5Temp4 = document.getElementById("day5Temp4");
+const day5Temp5 = document.getElementById("day5Temp5");
+const day5Temp6 = document.getElementById("day5Temp6");
+const day5Temp7 = document.getElementById("day5Temp7");
+const day5Temp8 = document.getElementById("day5Temp8");
 const day5Description = document.getElementById("day5Description");
-const day5Humidity = document.getElementById("day5Humidity");
-const day5Pressure = document.getElementById("day5Pressure");
 
 // variables for fetch
 let weather = [];
@@ -128,87 +143,86 @@ function insertData() {
   tomorrow.innerText = `${
     Days[new Date(newWeatherData[0].dt * 1000).getDay()]
   }`;
-  tomorrowAverage.innerText = `${(
+  tomorrowTemp1.innerText = `${(
     newWeatherData[0].main.temp - 273.15
   ).toFixed()}°`;
-  //   tomorrowHigh.innerText = `H: ${(
-  //     newWeatherData[0].main.temp_max - 273.15
-  //   ).toFixed()}°`;
-  //   tomorrowLow.innerText = `L: ${(
-  //     newWeatherData[0].main.temp_min - 273.15
-  //   ).toFixed()}°`;
-  tomorrowHumidity.innerText = `${newWeatherData[0].main.humidity}%`;
+  tomorrowTemp2.innerText = `${(
+    newWeatherData[1].main.temp - 273.15
+  ).toFixed()}°`;
+  tomorrowTemp3.innerText = `${(
+    newWeatherData[2].main.temp - 273.15
+  ).toFixed()}°`;
+  tomorrowTemp4.innerText = `${(
+    newWeatherData[3].main.temp - 273.15
+  ).toFixed()}°`;
+  tomorrowTemp5.innerText = `${(
+    newWeatherData[4].main.temp - 273.15
+  ).toFixed()}°`;
+  tomorrowTemp6.innerText = `${(
+    newWeatherData[5].main.temp - 273.15
+  ).toFixed()}°`;
+  tomorrowTemp7.innerText = `${(
+    newWeatherData[6].main.temp - 273.15
+  ).toFixed()}°`;
+  tomorrowTemp8.innerText = `${(
+    newWeatherData[7].main.temp - 273.15
+  ).toFixed()}°`;
   tomorrowDescription.innerText = `${
     newWeatherData[0].weather[0].description[0].toUpperCase() +
     newWeatherData[0].weather[0].description.slice(1)
   }`;
-  tomorrowPressure.innerText = `${newWeatherData[0].main.pressure} hPa`;
 
   day2.innerText = `${Days[new Date(newWeatherData[8].dt * 1000).getDay()]}`;
-  day2Average.innerText = `${(
-    newWeatherData[8].main.temp - 273.15
-  ).toFixed()}°`;
-  //   day2High.innerText = `H: ${(
-  //     newWeatherData[8].main.temp_max - 273.15
-  //   ).toFixed()}°`;
-  //   day2Low.innerText = `L: ${(
-  //     newWeatherData[8].main.temp_min - 273.15
-  //   ).toFixed()}°`;
-  day2Humidity.innerText = `${newWeatherData[8].main.humidity}%`;
+  day2Temp1.innerText = `${(newWeatherData[8].main.temp - 273.15).toFixed()}°`;
+  day2Temp2.innerText = `${(newWeatherData[9].main.temp - 273.15).toFixed()}°`;
+  day2Temp3.innerText = `${(newWeatherData[10].main.temp - 273.15).toFixed()}°`;
+  day2Temp4.innerText = `${(newWeatherData[11].main.temp - 273.15).toFixed()}°`;
+  day2Temp5.innerText = `${(newWeatherData[12].main.temp - 273.15).toFixed()}°`;
+  day2Temp6.innerText = `${(newWeatherData[13].main.temp - 273.15).toFixed()}°`;
+  day2Temp7.innerText = `${(newWeatherData[14].main.temp - 273.15).toFixed()}°`;
+  day2Temp8.innerText = `${(newWeatherData[15].main.temp - 273.15).toFixed()}°`;
   day2Description.innerText = `${
     newWeatherData[8].weather[0].description[0].toUpperCase() +
     newWeatherData[8].weather[0].description.slice(1)
   }`;
-  day2Pressure.innerText = `${newWeatherData[8].main.pressure} hPa`;
 
   day3.innerText = `${Days[new Date(newWeatherData[16].dt * 1000).getDay()]}`;
-  day3Average.innerText = `${(
-    newWeatherData[16].main.temp - 273.15
-  ).toFixed()}°`;
-  //   day3High.innerText = `H: ${(
-  //     newWeatherData[16].main.temp_max - 273.15
-  //   ).toFixed()}°`;
-  //   day3Low.innerText = `L: ${(
-  //     newWeatherData[16].main.temp_min - 273.15
-  //   ).toFixed()}°`;
-  day3Humidity.innerText = `${newWeatherData[16].main.humidity}%`;
+  day3Temp1.innerText = `${(newWeatherData[16].main.temp - 273.15).toFixed()}°`;
+  day3Temp2.innerText = `${(newWeatherData[17].main.temp - 273.15).toFixed()}°`;
+  day3Temp3.innerText = `${(newWeatherData[18].main.temp - 273.15).toFixed()}°`;
+  day3Temp4.innerText = `${(newWeatherData[19].main.temp - 273.15).toFixed()}°`;
+  day3Temp5.innerText = `${(newWeatherData[20].main.temp - 273.15).toFixed()}°`;
+  day3Temp6.innerText = `${(newWeatherData[21].main.temp - 273.15).toFixed()}°`;
+  day3Temp7.innerText = `${(newWeatherData[22].main.temp - 273.15).toFixed()}°`;
+  day3Temp8.innerText = `${(newWeatherData[23].main.temp - 273.15).toFixed()}°`;
   day3Description.innerText = `${
     newWeatherData[16].weather[0].description[0].toUpperCase() +
     newWeatherData[16].weather[0].description.slice(1)
   }`;
-  day3Pressure.innerText = `${newWeatherData[16].main.pressure} hPa`;
 
   day4.innerText = `${Days[new Date(newWeatherData[24].dt * 1000).getDay()]}`;
-  day4Average.innerText = `${(
-    newWeatherData[24].main.temp - 273.15
-  ).toFixed()}°`;
-  //   day4High.innerText = `H: ${(
-  //     newWeatherData[24].main.temp_max - 273.15
-  //   ).toFixed()}°`;
-  //   day4Low.innerText = `L: ${(
-  //     newWeatherData[24].main.temp_min - 273.15
-  //   ).toFixed()}°`;
-  day4Humidity.innerText = `${newWeatherData[24].main.humidity}%`;
+  day4Temp1.innerText = `${(newWeatherData[24].main.temp - 273.15).toFixed()}°`;
+  day4Temp2.innerText = `${(newWeatherData[25].main.temp - 273.15).toFixed()}°`;
+  day4Temp3.innerText = `${(newWeatherData[26].main.temp - 273.15).toFixed()}°`;
+  day4Temp4.innerText = `${(newWeatherData[27].main.temp - 273.15).toFixed()}°`;
+  day4Temp5.innerText = `${(newWeatherData[28].main.temp - 273.15).toFixed()}°`;
+  day4Temp6.innerText = `${(newWeatherData[29].main.temp - 273.15).toFixed()}°`;
+  day4Temp7.innerText = `${(newWeatherData[30].main.temp - 273.15).toFixed()}°`;
+  day4Temp8.innerText = `${(newWeatherData[31].main.temp - 273.15).toFixed()}°`;
   day4Description.innerText = `${
     newWeatherData[24].weather[0].description[0].toUpperCase() +
     newWeatherData[24].weather[0].description.slice(1)
   }`;
-  day4Pressure.innerText = `${newWeatherData[24].main.pressure} hPa`;
 
   day5.innerText = `${Days[new Date(newWeatherData[32].dt * 1000).getDay()]}`;
-  day5Average.innerText = `${(
-    newWeatherData[32].main.temp - 273.15
-  ).toFixed()}°`;
-  //   day5High.innerText = `H: ${(
-  //     newWeatherData[32].main.temp_max - 273.15
-  //   ).toFixed()}°`;
-  //   day5Low.innerText = `L: ${(
-  //     newWeatherData[32].main.temp_min - 273.15
-  //   ).toFixed()}°`;
-  day5Humidity.innerText = `${newWeatherData[32].main.humidity}%`;
+  day5Temp1.innerText = `${(newWeatherData[32].main.temp - 273.15).toFixed()}°`;
+  day5Temp2.innerText = `${(newWeatherData[33].main.temp - 273.15).toFixed()}°`;
+  day5Temp3.innerText = `${(newWeatherData[34].main.temp - 273.15).toFixed()}°`;
+  day5Temp4.innerText = `${(newWeatherData[35].main.temp - 273.15).toFixed()}°`;
+  day5Temp5.innerText = `${(newWeatherData[36].main.temp - 273.15).toFixed()}°`;
+  day5Temp6.innerText = `${(newWeatherData[37].main.temp - 273.15).toFixed()}°`;
   day5Description.innerText = `${
     newWeatherData[32].weather[0].description[0].toUpperCase() +
     newWeatherData[32].weather[0].description.slice(1)
   }`;
-  day5Pressure.innerText = `${newWeatherData[32].main.pressure} hPa`;
 }
